f689ea2f103101b9559bc9539227a0f5371e98c8
Sangsu Song <dvlprsong@gmail.com>
2020-11-28 00:08:12 +0900

    Subscribe Redux store to paint todo list on change

--- src/todo.js
+++ src/todo.js
@@ -2,28 +2,45 @@ import { createStore } from 'redux';
 
 const input = document.getElementById('input');
 const form = document.getElementById('todo-form');
+const list = document.getElementById('list');
 
 const ADD_TODO = 'ADD_TODO';
 
 const todoModifier = (todo = [], action) => {
-  console.log(action);
+  // console.log(action);
 
   switch (action.type) {
     case ADD_TODO:
-      return [...todo, input.value];
+      return [...todo, { text: action.text, id: action.id }];
     default:
       return todo;
   }
 }
 
 const todoStore = createStore(todoModifier);
+const paintTodo = () => {
+  list.innerHTML = '';
+
+  const todo = todoStore.getState();
+  todo.forEach(item => {
+    const li = document.createElement('li');
+    li.id = item.id;
+    li.textContent = item.text;
+    list.appendChild(li);
+  })
+}
+todoStore.subscribe(paintTodo);
+
+const addTodo = text => {
+  todoStore.dispatch({ type: ADD_TODO, text, id: Date.now() });
+}
 
 const handleSubmit = e => {
   e.preventDefault();
 
   const todo = input.value;
   input.value = '';
-  todoStore.dispatch({ type: ADD_TODO, todo });
+  addTodo(todo);
 }
 
 const initTodo = () => {
