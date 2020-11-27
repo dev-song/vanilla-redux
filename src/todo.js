ec0893bd060998f728b4cd3f2ea0e859e2fb163c
Sangsu Song <dvlprsong@gmail.com>
2020-11-28 00:25:09 +0900

    Add deleteTodo to Todo list with Redux

--- src/todo.js
+++ src/todo.js
@@ -5,6 +5,18 @@ const form = document.getElementById('todo-form');
 const list = document.getElementById('list');
 
 const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const addTodo = text => ({
+  type: ADD_TODO,
+  text,
+  id: Date.now()
+})
+
+const deleteTodo = id => ({
+  type: DELETE_TODO,
+  id
+});
 
 const todoModifier = (todo = [], action) => {
   // console.log(action);
@@ -12,9 +24,20 @@ const todoModifier = (todo = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       return [...todo, { text: action.text, id: action.id }];
+    case DELETE_TODO:
+      return [...todo.filter(item => item.id !== action.id)];
     default:
       return todo;
   }
+}
+
+const dispatchAddTodo = text => {
+  todoStore.dispatch(addTodo(text));
+}
+
+const dispatchDeleteTodo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  todoStore.dispatch(deleteTodo(id));
 }
 
 const todoStore = createStore(todoModifier);
@@ -24,23 +47,23 @@ const paintTodo = () => {
   const todo = todoStore.getState();
   todo.forEach(item => {
     const li = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'DEL';
+    deleteBtn.addEventListener('click', dispatchDeleteTodo);
     li.id = item.id;
     li.textContent = item.text;
+    li.appendChild(deleteBtn);
     list.appendChild(li);
   })
 }
 todoStore.subscribe(paintTodo);
-
-const addTodo = text => {
-  todoStore.dispatch({ type: ADD_TODO, text, id: Date.now() });
-}
 
 const handleSubmit = e => {
   e.preventDefault();
 
   const todo = input.value;
   input.value = '';
-  addTodo(todo);
+  dispatchAddTodo(todo);
 }
 
 const initTodo = () => {
