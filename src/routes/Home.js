ba5402298df7142c1fe187f693e54d6bb2335fe8
Sangsu Song <dvlprsong@gmail.com>
2020-11-29 23:33:36 +0900

    Remake To do list with React

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,32 @@
-export default () => 'Home';
+import React, { useState } from 'react';
+
+function Home() {
+  const [text, setText] = useState('');
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+
+    // Do something with event or variables
+  }
+
+  return (
+    <>
+      <h1>To-do List</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={onChange}
+          placeholder="Write a to-do here"
+        />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+export default Home;
