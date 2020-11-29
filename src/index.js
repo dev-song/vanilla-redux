b9481e0fd5781242d4b2e644a83bb2f67bde21a3
Sangsu Song <dvlprsong@gmail.com>
2020-11-29 23:26:15 +0900

    Disable legacy vanilla codes and activate React app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/*
 import initCounter from './counter';
 import initTodo from './todo';
 
 initCounter();
 initTodo();
+*/
