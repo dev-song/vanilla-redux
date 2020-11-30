a9e419906cc29656d3cec78df53370a20124b42c
Sangsu Song <dvlprsong@gmail.com>
2020-11-30 22:35:17 +0900

    Add react-router link to details page

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { actionCreators } from '../store';
 
-function Todo({ text, onBtnClick }) {
+function Todo({ text, id, onBtnClick }) {
   return (
     <li>
-      {text}
-      <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>
+        {text}
+        <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   )
 }
